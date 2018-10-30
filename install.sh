e7a8a57a2cd88b7b815814bf8ffa24b3072160e3
aliraslan <alinour1998@gmail.com>
2018-10-30 05:23:44 +0200

    empty

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mv playdl.sh /usr/bin/playdl
-mv Playdl.desktop /usr/share/applications
+mv playdl.sh ~/.local/usr/bin/playdl
+mv Playdl.desktop ~/.local/share/applications
 cd ..
 rm -rf playdl
